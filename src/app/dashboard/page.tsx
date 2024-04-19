a0ec6d5c07f640fbcbeba8de2d649f30926170a1
tropssap <57821127+tropssap@users.noreply.github.com>
2024-04-19 10:40:16 +0300

    fixed File appears to be binary

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,8 @@
-import Link from "next/link";
+
 import { PointsTable } from "~/components/component/points-table";
 import { ScrollArea } from "~/components/ui/scroll-area";
-// import { createClient } from "~/server/supabase/supabase";
 
 export default async function HomePage() {
-  // const supabase = createClient();
-
-  // const {
-  //   data: { user },
-  // } = await supabase.auth.getUser();
 
   return (
     <main className="flex  flex-col items-center justify-center overflow-hidden">
